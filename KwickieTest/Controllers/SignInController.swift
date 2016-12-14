cead3332919e70cce917b097694f69cc172c550a
Nishant Shrestha <nishant@Nishants-MacBook-Pro.local>
2016-12-14 21:58:26 +1000

    Initial commit.

---
+++ KwickieTest/Controllers/SignInController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInController.swift
+//  KwickieTest
+//
+//  Created by Nishant Shrestha on 14/12/16.
+//  Copyright © 2016 Nishant Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class SignInController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
