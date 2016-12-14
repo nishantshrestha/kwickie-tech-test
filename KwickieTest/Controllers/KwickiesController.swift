4dd70e1c5bd000698188da089591c3740d893df8
Nishant Shrestha <nishant@Nishants-MacBook-Pro.local>
2016-12-14 22:34:36 +1000

    Added static function to initialise KwickiesController from storyboard file.

--- KwickieTest/Controllers/KwickiesController.swift
+++ KwickieTest/Controllers/KwickiesController.swift
@@ -15,3 +15,22 @@ class KwickiesController: UITableViewController {
     }
 
 }
+
+
+// MARK: Tableview datasource and delegate extension
+extension KwickiesController {
+    
+}
+
+// MARK: Storyboard based instantiation extension
+// From Stan Ostrovskiy's Medium Post - https://medium.com/ios-os-x-development/xcode-a-better-way-to-deal-with-storyboards-8b6a8b504c06#.pa7bkv7cw
+extension KwickiesController {
+    static func storyboardInstance() -> KwickiesController? {
+        let className = String(describing: self)
+        
+        let storyboard = UIStoryboard(name: className, bundle: nil)
+        
+        return storyboard.instantiateInitialViewController() as? KwickiesController
+        
+    }
+}
