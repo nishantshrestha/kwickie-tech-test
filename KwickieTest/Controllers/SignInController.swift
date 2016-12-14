2623c2dc5fc17ad9d88a53b9d87ad29ba0500250
Nishant Shrestha <nishant@nishants-macbook-pro.local>
2016-12-14 22:50:11 +1000

    Added function to make member login API call on button click.

--- KwickieTest/Controllers/SignInController.swift
+++ KwickieTest/Controllers/SignInController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import JGProgressHUD
 
 class SignInController: UIViewController {
 
@@ -16,4 +17,37 @@ class SignInController: UIViewController {
         super.viewDidLoad()
     }
 
+    @IBAction func signInButtonPressed(_ sender: AnyObject) {
+        
+        // Create the parameters based on the user input
+        let params: Parameters = [
+            "email": "kevin.jonas@kwickie.com",
+            "password": "KwickieRocks"
+        ]
+        
+        signInWithParameters(params: params) { (status, authToken) in
+            // If the status was success, then proceed.
+            if status {
+                
+            } else { // Else show an error message
+                
+            }
+        }
+    }
+    
+    func signInWithParameters(params: Parameters, completionHandler: (Bool, String) -> Void) {
+        
+        let loginURL = "https://bigdev.kwickie.com/api/members/login"
+        
+        // Perform network request
+        Alamofire.request(loginURL, method: .post, parameters: params, encoding: JSONEncoding.default)
+            .validate()
+            .responseJSON { [weak self] response in
+                if let data = response.data {
+                    let json = JSON(data: data)
+                    
+                    print(json)
+                }
+        }
+    }
 }
